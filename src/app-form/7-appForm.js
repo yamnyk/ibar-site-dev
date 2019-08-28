85b640227a2acf607cbeaae985c2fc7e926aafa2
yamnyk <yamnyk.vlog@gmail.com>
2019-08-28 17:48:53 +0300

    form works by submit, no styles

--- src/app-form/7-appForm.js
+++ src/app-form/7-appForm.js
@@ -1,19 +1,29 @@
 const form = document.querySelector('#appForm');
 form.addEventListener('submit', (e) => {
-  const name = document.querySelector('#formName').value;
-  const age = document.querySelector('#formAge').value;
-  const tel = document.querySelector('#formPhone').value;
-  const email = document.querySelector('#formMail').value;
-  const program = document.querySelector('#formProgram').value;
-  const letter = document.querySelector('#formLetter').value;
+  e.preventDefault();
+  const name = document.querySelector('#formName');
+  const age = document.querySelector('#formAge');
+  const tel = document.querySelector('#formPhone');
+  const email = document.querySelector('#formMail');
+  const program = document.querySelector('#formProgram');
+  const letter = document.querySelector('#formLetter');
 
   const url = new URL('https://script.google.com/macros/s/AKfycbzKBJ4yX6b8mwtzJmaPtGew0wOCthTeceoXNAidAJE53y_vsA/exec');
-  url.searchParams.append('name', name);
-  url.searchParams.append('age', age);
-  url.searchParams.append('tel', tel);
-  url.searchParams.append('mail', email);
-  url.searchParams.append('program', program);
-  url.searchParams.append('letter', letter);
+  url.searchParams.append('name', name.value);
+  url.searchParams.append('age', age.value);
+  url.searchParams.append('tel', tel.value);
+  url.searchParams.append('mail', email.value);
+  url.searchParams.append('program', program.value);
+  url.searchParams.append('letter', letter.value);
 
-  fetch(url);
+  fetch(url).then((res) => {
+    console.log('Hello');
+  });
+
+  name.value = '';
+  age.value = '';
+  tel.value = '';
+  email.value = '';
+  program.value = '';
+  letter.value = '';
 });
