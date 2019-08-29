3d88b2566713ea1d99437bad81e06b0ed1f427f5
Nik Yamnyk <38811365+yamnyk@users.noreply.github.com>
2019-08-29 16:59:31 +0300

    From export values into google exel (#7)
    * create appform js
    
    * add test replace font, add form listener fetching to google spreadsheet
    
    * refactor font face
    
    * refactor font face
    
    * form works by submit, no styles
    
    * form works by submit, no styles

---
+++ src/app-form/7-appForm.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('#appForm');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = document.querySelector('#formName');
+  const age = document.querySelector('#formAge');
+  const tel = document.querySelector('#formPhone');
+  const email = document.querySelector('#formMail');
+  const program = document.querySelector('#formProgram');
+  const letter = document.querySelector('#formLetter');
+
+  const url = new URL('https://script.google.com/macros/s/AKfycbzKBJ4yX6b8mwtzJmaPtGew0wOCthTeceoXNAidAJE53y_vsA/exec');
+  url.searchParams.append('name', name.value);
+  url.searchParams.append('age', age.value);
+  url.searchParams.append('tel', tel.value);
+  url.searchParams.append('mail', email.value);
+  url.searchParams.append('program', program.value);
+  url.searchParams.append('letter', letter.value);
+
+  fetch(url).then((res) => {
+    document.querySelector('.success-modal').style.display = 'flex';
+  }, (error) => {
+    console.dir(error)
+  });
+
+  name.value = '';
+  age.value = '';
+  tel.value = '';
+  email.value = '';
+  program.value = '';
+  letter.value = '';
+});
