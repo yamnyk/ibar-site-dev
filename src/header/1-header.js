4981a5d0171dce9315663cc5655e4134cfe7c3f2
velykyi <petro.velykyi@gmail.com>
2019-09-04 19:34:50 +0300

    Menu links, bag fixed in JS.

--- src/header/1-header.js
+++ src/header/1-header.js
@@ -1,30 +1,29 @@
 // ************************************************************************
 // Handle smooth scrolling to section
 function scrollToSection(element) {
-    // Menu height correction in px
-    const menuHeight = 68;
-    window.scroll(
-      {
-          top: element.offsetTop - menuHeight,
-          left: 0,
-          behavior: 'smooth'
-      })
+  // Menu height correction in px
+  const menuHeight = 68;
+  window.scroll(
+    {
+      top: element.offsetTop - menuHeight,
+      left: 0,
+      behavior: 'smooth'
+    })
 }
 
 // Listener on menu item
-document.querySelector('.navbar__content').addEventListener('click',  function (e) {
-    e.preventDefault();
-    if (e.target.closest('A')) {
-        scrollToSection(document.getElementById(e.target.hash.slice(1)));
-    }
-});
+// document.querySelector('.navbar__content a').addEventListener('click',  function (e) {
 
-// Listener on button
-document.querySelector('a.application').addEventListener('click',  function (e) {
+// });
+
+// Listener on buttons "Make-request"
+document.querySelectorAll('a.make-request').forEach((el)=>{
+  el.addEventListener('click', function (e) {
     e.preventDefault();
     if (e.target.closest('A')) {
-        scrollToSection(document.getElementById(e.target.hash.slice(1)));
+      scrollToSection(document.getElementById(e.target.hash.slice(1)));
     }
+  })
 });
 
 
@@ -32,43 +31,47 @@ document.querySelector('a.application').addEventListener('click',  function (e) 
 // Handle Change Languages
 const langList = document.getElementById('navbar__lang-list');
 langList.addEventListener('change', () => {
-    switch (langList.value) {
-        case 'ru':
-            window.location = '/index.html';
-            break;
-        case 'az':
-            window.location = '/index_az.html';
-            break;
-        case 'en':
-            window.location = '/index_en.html';
-            break;
-        default:
-            console.log('Unknown language!');
-    }
+  switch (langList.value) {
+    case 'ru':
+      window.location = '/index.html';
+      break;
+    case 'az':
+      window.location = '/index_az.html';
+      break;
+    case 'en':
+      window.location = '/index_en.html';
+      break;
+    default:
+      console.log('Unknown language!');
+  }
 });
 
 // ************************************************************************
 // Handle burger menu
 function uncheckBurgerMenu() {
-    document.getElementById('navbar__checkbox').checked = false;
+  document.getElementById('navbar__checkbox').checked = false;
 }
 
 function hideMenu(e) {
-    if (e.target.closest('A')) {
-        uncheckBurgerMenu();
-    }
+  if (e.target.closest('A')) {
+    uncheckBurgerMenu();
+  }
 }
 
-document.querySelector('.navbar__menu-wrapper').addEventListener('click',  (e) => {
-    if (document.body.clientWidth < 981) {
-        hideMenu(e);
-    }
+document.querySelector('.navbar__menu-wrapper').addEventListener('click', (e) => {
+  e.preventDefault();
+  if (e.target.closest('A')) {
+    scrollToSection(document.getElementById(e.target.hash.slice(1)));
+  }
+  if (document.body.clientWidth < 981) {
+    hideMenu(e);
+  }
 
 });
 
 window.addEventListener('resize', () => {
-    if (document.body.clientWidth > 979) {
-        uncheckBurgerMenu();
-    }
+  if (document.body.clientWidth > 979) {
+    uncheckBurgerMenu();
+  }
 });
 
