8371a66f8521659c8dc57dc2f10134b1bd094091
Golik_Andrii <Golik.Andrii@gmail.com>
2019-09-06 01:11:58 +0300

    Add FAQ section. Mobile.

--- src/faq/faq.js
+++ src/faq/faq.js
@@ -1,6 +1,6 @@
 function toogle(e) {
-    var elems = document.getElementsByClassName('faq-header-title');
-    for (var i = 0; i < elems.length; i += 1) {
+    let elems = document.getElementsByClassName('faq-header-title');
+    for (let i = 0; i < elems.length; i += 1) {
         elems[i].childNodes[3].style.display = 'none';
     }
     e.childNodes[3].style.display = 'block';
